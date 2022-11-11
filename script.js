56969a251151e738279eb255b91e872a1ca8473b
Rickson Figueira <sadrisco@outlook.com>
2022-11-11 16:41:56 -0300

    Updated image formats

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const imageArray = [
-  "./asset/Beatriz_Campelo.png",
-  "./asset/Elisa_Rodrigues_.png",
-  "./asset/Helena_Maria.png",
-  "./asset/Henrique_da_Cruz.png",
-  "./asset/Louise_Cristina.png",
-  "./asset/Marcos_Menine.png",
-  "./asset/Maria_Eunice.png",
-  "./asset/Natan_Batissoco.png",
-  "./asset/Raphaella_Lemos.png",
-  "./asset/Rebeca_Noemi.png",
-  "./asset/Tom_Alerton.png"
+  "./asset/Beatriz_Campelo.jpeg",
+  "./asset/Elisa_Rodrigues_.jpeg",
+  "./asset/Helena_Maria.jpeg",
+  "./asset/Henrique_da_Cruz.jpeg",
+  "./asset/Louise_Cristina.jpeg",
+  "./asset/Marcos_Menine.jpeg",
+  "./asset/Maria_Eunice.jpeg",
+  "./asset/Natan_Batissoco.jpeg",
+  "./asset/Raphaella_Lemos.jpeg",
+  "./asset/Rebeca_Noemi.jpeg",
+  "./asset/Tom_Alerton.jpeg"
 ];
 
 const image = document.getElementById("figurinha");
