bb251ce0f77f4da3fd16ba9c8ef706c374f02dbf
Rickson Figueira <sadrisco@outlook.com>
2022-11-11 12:24:14 -0300

    deleted old assets

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 const imageArray = [
-  "./asset/cop27-figurinha1.jpg",
-  "./asset/cop27-figurinha2.jpg",
-  "./asset/cop27-figurinha3.jpg",
   "./asset/Beatriz_Campelo.png",
   "./asset/Elisa_Rodrigues_.png",
   "./asset/Helena_Maria.png",
